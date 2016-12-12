2f4343aece0508c66b831d081c70bad26e0d3fcf
OllieMartin <olivermartin410@yahoo.co.uk>
2016-12-12 23:45:29 +0000

    Predictive shooting

--- MyRobots/src/spacecadets2016/JammyDodgerV.java
+++ MyRobots/src/spacecadets2016/JammyDodgerV.java
@@ -22,6 +22,7 @@ import robocode.RobocodeFileOutputStream;
 import robocode.RoundEndedEvent;
 import robocode.Rules;
 import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
 
 public class JammyDodgerV extends AdvancedRobot {
 
@@ -29,7 +30,8 @@ public class JammyDodgerV extends AdvancedRobot {
 	private Map<String,RoboHitData> enemyData;
 	private Map<Bullet,String> bulletTargets;
 	Location dangerZone = null;
-	
+
+	byte moveDirection = 1;
 	double lastBearing;
 	boolean onWall;
 	double latestDistance;
@@ -43,40 +45,39 @@ public class JammyDodgerV extends AdvancedRobot {
 	double averageHitDistance;
 	int opponents;
 	double wallTurn;
-	
+
 	boolean normal;
-    boolean peek; // Don't turn if there's a robot there
-    double moveAmount; // How much to move
-    
-    final static int TRIGGER = 1000;
-	
+	boolean peek; // Don't turn if there's a robot there
+	double moveAmount; // How much to move
+
+	final static int TRIGGER = 1000;
+
 	/**
 	 * run: Test's default behavior
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void run() {
-		
+
 		System.out.println("Jammy Dodger Mark V - Loading");
-		
+
 		enemyData = new HashMap<String,RoboHitData>();
-		
+
 		File file = getDataFile("output.dat");
 		try {
 			FileInputStream stream = new FileInputStream(file);
-			
-			FileReader fr = new FileReader(file);
+
 			//BufferedReader br = new BufferedReader(stream);
 			ObjectInputStream ois = new ObjectInputStream(stream);
 			enemyData = (Map<String, RoboHitData>) ois.readObject();
 			ois.close();
-			
+
 		} catch (IOException e) {
 			System.out.println("Could not load previous file!");
 		} catch (ClassNotFoundException e) {
 			System.out.println("ClassNotFound!");
 		}
-		
+
 		/*setAdjustRadarForGunTurn(true);
 		setAdjustRadarForRobotTurn(true);
 		setAdjustGunForRobotTurn(true);*/
@@ -85,26 +86,26 @@ public class JammyDodgerV extends AdvancedRobot {
 		bulletTargets = new HashMap<Bullet,String>();
 		enabled = true;
 		setColors(Color.getHSBColor(57, 18, 10),Color.red,Color.red); // body,gun,radar
-		
+
 		System.out.println("Enabled!");
-		
+
 		opponents = getOthers();
-		
+
 		if (getOthers() > TRIGGER) {
 			// Initialize moveAmount to the maximum possible for this battlefield.
-	        moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
-	        // Initialize peek to false
-	        peek = false;
+			moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
+			// Initialize peek to false
+			peek = false;
 
-	        // turnLeft to face a wall.
-	        // getHeading() % 90 means the remainder of
-	        // getHeading() divided by 90.
-	        turnLeft(getHeading() % 90);
-	        ahead(moveAmount);
-	        // Turn the gun to turn right 90 degrees.
-	        peek = true;
-	        turnGunRight(90);
-	        turnRight(90);
+			// turnLeft to face a wall.
+			// getHeading() % 90 means the remainder of
+			// getHeading() divided by 90.
+			turnLeft(getHeading() % 90);
+			ahead(moveAmount);
+			// Turn the gun to turn right 90 degrees.
+			peek = true;
+			turnGunRight(90);
+			turnRight(90);
 		}
 
 		// Robot main loop
@@ -144,17 +145,17 @@ public class JammyDodgerV extends AdvancedRobot {
 			PrintStream ps = new PrintStream(stream);
 			//FileWriter fw = new FileWriter(stream);
 			ps.print(enemyData);
-			
+
 			ps.close();
 			System.out.println("SAVED!");
 			System.out.println(file.getAbsolutePath());
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+
 	/*@Override
 	public void onWin(WinEvent event) {
 		File file = getDataFile("output.dat");
@@ -162,18 +163,18 @@ public class JammyDodgerV extends AdvancedRobot {
 			RobocodeFileOutputStream stream = new RobocodeFileOutputStream(file);
 			PrintStream ps = new PrintStream(stream);
 			ps.print(enemyData);
-			
+
 			ps.close();
 			System.out.println("SAVED!");
 			System.out.println(file.getAbsolutePath());
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}*/
-	
-	
+
+
 	@Override
 	public void onBulletHit(BulletHitEvent event) {
 		RoboHitData temp;
@@ -186,7 +187,7 @@ public class JammyDodgerV extends AdvancedRobot {
 		}
 		super.onBulletHit(event);
 	}
-	
+
 	@Override
 	public void onBulletMissed(BulletMissedEvent event) {
 		RoboHitData temp;
@@ -199,7 +200,7 @@ public class JammyDodgerV extends AdvancedRobot {
 		}
 		super.onBulletMissed(event);
 	}
-	
+
 	@Override
 	public void onBulletHitBullet(BulletHitBulletEvent event) {
 		RoboHitData temp;
@@ -212,128 +213,153 @@ public class JammyDodgerV extends AdvancedRobot {
 		}
 		super.onBulletHitBullet(event);
 	}
-	
+
 	private void wallSkim() {
-              // Look before we turn when ahead() completes.
-              peek = true;
-              // Move up the wall
-              setAhead(moveAmount);
-              // Don't look now
-              peek = false;
-              // Turn to the next wall
-              setTurnRight(90);
-              execute();
+		// Look before we turn when ahead() completes.
+		peek = true;
+		// Move up the wall
+		setAhead(moveAmount);
+		// Don't look now
+		peek = false;
+		// Turn to the next wall
+		setTurnRight(90);
+		execute();
 	}
-	
+
 	private void basicSeek() {
 		//ahead(100);
 		turnRadarRight(360);
-		if (!noTurn) {
+
 		if (!retreat) {
 			toMove = toMove + latestDistance/2;
 			setAhead(toMove);
 			setTurnRight(toTurn);
 		} else {
+			//setTurnRight(20);
 			setAhead(toMove);
 			retreat = false;
 		}
-		}
 		
-		if (noTurn) {
-			if (!(getX() > getBattleFieldWidth() - 50 || getY() > getBattleFieldHeight() - 50 || getX() < 50 || getY() < 50)) {
-				noTurn = false;
-				
+		if (!onWall) {
+			if (getX() > getBattleFieldWidth()-(getBattleFieldWidth()/15) && getHeading() >= 0 && getHeading() <= 180) {
+				toTurn += toTurn/2;
+				toMove -= toMove/2;
+			} else if (getX() > getBattleFieldWidth()-(getBattleFieldWidth()/15) ) {
+				//
+			} else if (getX() < (getBattleFieldWidth()/15) && getHeading() >= 180 && getHeading() <= 360) {
+				toTurn += toTurn/2;
+				toMove -= toMove/2;
+			} else if(getX() < (getBattleFieldWidth()/15) ) {
+				//
+			} else if (getY() > getBattleFieldHeight()-(getBattleFieldHeight()/15) && (getHeading() >= 270 && getHeading() <= 360) || (getHeading() >= 0 && getHeading() <= 90)) {
+				toTurn += toTurn/2;
+				toMove -= toMove/2;
+			} else if (getY() > getBattleFieldHeight()-(getBattleFieldHeight()/15)) {
+				//
+			} else if (getY() < (getBattleFieldHeight()/15) && (getHeading() >= 90 && getHeading() <= 270) ) {
+				toTurn += toTurn/2;
+				toMove -= toMove/2;
+			} else if (getY() < (getBattleFieldHeight()/15)){
+				//
 			}
-			if (getDistanceRemaining() == 0) {
-				setAhead(100);
-			}
-			setTurnRight(wallTurn);
 		}
+
 		execute();
 		toMove = 0;
 		toTurn = 0;
+
+		if (getHitWallEvents().size() == 0) {
+			onWall = false;
+		}
 	}
-	
+
 	private void basicEvade() {
 		setTurnRadarRight(90);
 		int aheadVal = 100;
 		if (!noTurn) {
-		if (dangerZone == null) {
-			toTurn = toTurn + (90*Math.random());
-		} else {
-			if (Math.abs(getX() - dangerZone.x) < 200 && Math.abs(getY() - dangerZone.y) < 200) {
-				toTurn = (dangerZone.getBearing(getX(), getY(), getHeading()) + 180);
-				aheadVal = aheadVal + 100;
-			} else {
+			if (dangerZone == null) {
 				toTurn = toTurn + (90*Math.random());
+			} else {
+				if (Math.abs(getX() - dangerZone.x) < 200 && Math.abs(getY() - dangerZone.y) < 200) {
+					toTurn = (dangerZone.getBearing(getX(), getY(), getHeading()) + 180);
+					aheadVal = aheadVal + 100;
+				} else {
+					toTurn = toTurn + (90*Math.random());
+				}
+
 			}
-			
-		}
-		toMove = toMove + aheadVal;
-		setTurnRight(toTurn);
-		setAhead(toMove);
+			toMove = toMove + aheadVal;
+			setTurnRight(toTurn);
+			setAhead(toMove);
 		}
 		if (!onWall) {
-		if (getX() > getBattleFieldWidth()-(getBattleFieldWidth()/4) && getHeading() >= 0 && getHeading() <= 180) {
-			setTurnRight(90);
-		} else if (getX() > getBattleFieldWidth()-(getBattleFieldWidth()/4) ) {
-			setTurnRight(0);
-		} else if (getX() < (getBattleFieldWidth()/4) && getHeading() >= 180 && getHeading() <= 360) {
-			setTurnRight(90);
-		} else if(getX() < (getBattleFieldWidth()/4) ) {
-			setTurnRight(0);
-		} else if (getY() > getBattleFieldHeight()-(getBattleFieldHeight()/4) && (getHeading() >= 270 && getHeading() <= 360) || (getHeading() >= 0 && getHeading() <= 90)) {
-			setTurnRight(90);
-		} else if (getY() > getBattleFieldHeight()-(getBattleFieldHeight()/4)) {
-			setTurnRight(0);
-		} else if (getY() < (getBattleFieldHeight()/4) && (getHeading() >= 90 && getHeading() <= 270) ) {
-			setTurnRight(90);
-		} else if (getY() < (getBattleFieldHeight()/4)){
-			setTurnRight(0);
-		}
-		}
-		if (noTurn) {
-			if (!(getX() > getBattleFieldWidth() - 50 || getY() > getBattleFieldHeight() - 50 || getX() < 50 || getY() < 50)) {
-				noTurn = false;
+			if (getX() > getBattleFieldWidth()-(getBattleFieldWidth()/4) && getHeading() >= 0 && getHeading() <= 180) {
+				setTurnRight(90);
+			} else if (getX() > getBattleFieldWidth()-(getBattleFieldWidth()/4) ) {
+				setTurnRight(0);
+			} else if (getX() < (getBattleFieldWidth()/4) && getHeading() >= 180 && getHeading() <= 360) {
+				setTurnRight(90);
+			} else if(getX() < (getBattleFieldWidth()/4) ) {
+				setTurnRight(0);
+			} else if (getY() > getBattleFieldHeight()-(getBattleFieldHeight()/4) && (getHeading() >= 270 && getHeading() <= 360) || (getHeading() >= 0 && getHeading() <= 90)) {
+				setTurnRight(90);
+			} else if (getY() > getBattleFieldHeight()-(getBattleFieldHeight()/4)) {
+				setTurnRight(0);
+			} else if (getY() < (getBattleFieldHeight()/4) && (getHeading() >= 90 && getHeading() <= 270) ) {
+				setTurnRight(90);
+			} else if (getY() < (getBattleFieldHeight()/4)){
+				setTurnRight(0);
 			}
-			if (getDistanceRemaining() == 0) {
-				setAhead(100);
-			}
-			setTurnRight(wallTurn);
+		}
+		if (getHitWallEvents().size() == 0) {
+			onWall = false;
 		}
 		execute();
 		retreat = false;
 	}
-	
+
 	private Location getDangerZone() {
 		Location dangerZone = null;
-		
+
 		double avX = 0;
 		double avY = 0;
 		int counter = 0;
-		
+
 		for (Location l : enemyLocations.values()) {
 			avX = avX + l.x;
 			avY = avY + l.y;
 			counter++;
 		}
-		
+
 		avX = avX/counter;
 		avY = avY/counter;
-		
+
 		dangerZone = new Location(avX,avY);
-		
+
 		return dangerZone;
 	}
-	
+
 	private void fireScanned(Location l, double bearing, String name) {
+		double airTime = l.d/(20 - 3*Math.min(400/l.d,3));
+		//double travelDistance = l.v * airTime;
 		setAdjustRadarForGunTurn(true);
-		turnGunRight(getHeading() - getGunHeading() + bearing);
+		double fX;
+		double fY;
+		
+		fX = l.x + l.v * airTime * Math.sin(Math.toRadians(l.h)) - getX();
+		fY = l.y + l.v * airTime * Math.cos(Math.toRadians(l.h)) - getY();
+		
+		Location fL = new Location(fX,fY);
+		//turnGunRight(getHeading() - getGunHeading() + bearing);
+		double absoluteBearing = getHeadingRadians() + Math.toRadians(bearing);
+		setTurnGunRightRadians(Utils.normalRelativeAngle(absoluteBearing - 
+			    getGunHeadingRadians() + (l.v * Math.sin(Math.toRadians(l.h) - 
+			    absoluteBearing) / airTime)));
 		waitFor(new GunTurnCompleteCondition(this));
 		Bullet b = fireBullet(Math.min(400/l.d,3));
-		
+
 		bulletTargets.put(b, name);
-		
+
 	}
 
 	/**
@@ -352,59 +378,47 @@ public class JammyDodgerV extends AdvancedRobot {
 		// Replace the next line with any behavior you would like
 		latestDistance = e.getDistance();
 		lastBearing = e.getBearing();
-		/*if (e.getDistance() <= 100) {
-			fire(5);
-		} else if (e.getDistance() <= 150) {
-			fire(4);
-		} else if (e.getDistance() <= 150) {
-			fire(3);
-			//turnRight(this.getHeading() + e.getBearing());
-		} else if (e.getDistance() <= 300) {
-			//turnRight(this.getHeading() + e.getBearing());
-			//ahead(100);
-			fire(2);
-		} else if (e.getDistance() <= getBattleFieldWidth()/2){
-			//turnRight(this.getHeading() + e.getBearing());
-			//ahead(100);
-			fire(1);
-			//rapidFire(1,2);
-		}*/
-		
+
 		if (getOthers() > TRIGGER) {
-			 fire(2);
-	            if (peek) {
-	                  scan();
-	            }
-	            return;
+			fire(2);
+			if (peek) {
+				scan();
+			}
+			return;
 		}
 		
 		if (!(getEnergy() < 5) || e.getEnergy() < 20) {
-				if (enemyData.get(e.getName()).getAccuracy() < 20) {
-					if (e.getDistance() < enemyData.get(e.getName()).getAverageHitDistance() + 20) {
+			if (enemyData.get(e.getName()).getAccuracy() < 20) {
+				if (e.getDistance() < enemyData.get(e.getName()).getAverageHitDistance() + 20) {
+					fireScanned(enemyLocations.get(e.getName()),e.getBearing(), e.getName());
+				} else {
+					if (enemyData.get(e.getName()).hits == 0) {
 						fireScanned(enemyLocations.get(e.getName()),e.getBearing(), e.getName());
-					} else {
-						if (enemyData.get(e.getName()).hits == 0) {
-							fireScanned(enemyLocations.get(e.getName()),e.getBearing(), e.getName());
-						}
 					}
+				}
 			} else {
 				fireScanned(enemyLocations.get(e.getName()),e.getBearing(), e.getName());
 			}
 		}
 		//if (!(getOthers() > 1)) {
-			if (e.getBearing() > 0) {
-				toTurn = toTurn + (45 + 45 * Math.abs(Math.sin(e.getDistance())));
-			} else if (e.getBearing() < 0) {
-				toTurn = toTurn - (45 + 45* Math.abs(Math.sin(e.getDistance())));
-			}
+		//toTurn = toTurn + (45* moveDirection + 45*Math.random());
+		moveDirection *= -1;
+		if (e.getBearing() > 0) {
+			toTurn = toTurn + (90 + 45*Math.random());
+		} else if (e.getBearing() < 0) {
+			toTurn = toTurn - (90 + 45*Math.random());
+		}
 		//}
 		if (e.getEnergy() > getEnergy() + 10 && e.getDistance() < 100) {
 			System.out.println("[ALERT] Scanned robot has superior energy levels & is near - Retreat!");
 			//back(200);
-			toMove = -200;
+			
+			
+			//toMove = -200;
+			toMove *= -1;
 			retreat = true;
 		}
-		
+
 	}
 
 	/**
@@ -420,14 +434,14 @@ public class JammyDodgerV extends AdvancedRobot {
 			return;
 		}
 		if (!onWall) {
-		// Replace the next line with any behavior you would like
-		//turnRight(this.getHeading() + e.getBearing() + 45);
+			// Replace the next line with any behavior you would like
+			//turnRight(this.getHeading() + e.getBearing() + 45);
 			toTurn = (Math.random() * 45);
 			toMove = (Math.random()*50 + 50);
 		}
-		
+
 	}
-	
+
 	public void reAlign() {
 		System.out.println("[INFO] Realigning to last heading");
 		if (!enabled) {
@@ -435,27 +449,27 @@ public class JammyDodgerV extends AdvancedRobot {
 		}
 		setTurnGunRight(this.getHeading() + lastBearing);
 	}
-	
+
 	public void onHitEnemy(HitRobotEvent e) {
-		
+
 		if (getOthers() > TRIGGER) {
 			if (e.getBearing() > -90 && e.getBearing() < 90) {
-                back(100);
-          } // else he's in back of us, so set ahead a bit.
-          else {
-                ahead(100);
-          }
+				back(100);
+			} // else he's in back of us, so set ahead a bit.
+			else {
+				ahead(100);
+			}
 			return;
 		}
-		
+
 		if ((getOthers() > 1 && getEnergy()>50) || getOthers() == 1) {
-		turnGunRight(this.getHeading() - this.getGunHeading() + e.getBearing());
-		fire(Rules.MAX_BULLET_POWER);
-		toMove = -(10);
+			turnGunRight(this.getHeading() - this.getGunHeading() + e.getBearing());
+			setFire(Rules.MAX_BULLET_POWER);
+			toMove = -(20);
 		}
-		
+
 	}
-	
+
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
@@ -468,7 +482,8 @@ public class JammyDodgerV extends AdvancedRobot {
 		if (!enabled) {
 			return;
 		}
-		if (!onWall) {
+		onWall = true;
+		/*if (!onWall) {
 		onWall = true;
 		// Replace the next line with any behavior you would like
 		wallTurn = (e.getBearing() + 180 + Math.random()* 45);
@@ -481,7 +496,9 @@ public class JammyDodgerV extends AdvancedRobot {
 		setAhead(toMove);
 		toMove = 0;
 		onWall = false;
-		}
+		}*/
+		toTurn += 90;
+		toMove = 100;
 	}	
-	
+
 }
